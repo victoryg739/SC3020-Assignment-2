6ab37e40b31d3592a852d95595af7c432de211bc
Jin He <choojinhe@hotmail.com>
2023-11-13 14:10:19 +0800

    add tabs for block access and more info in tree

--- explore.py
+++ explore.py
@@ -1,6 +1,77 @@
 import psycopg2
 from config import db_host, db_name, db_user, db_password
+# from graphviz import Digraph
+from PyQt5.QtWidgets import  QTreeWidgetItem
 
+
+def get_execution_plan(query):
+    """
+    This function gets the execution plan for the given SQL query
+    and returns it as a string.
+    """
+    try:
+        # Connect to the database
+        conn = psycopg2.connect(
+            host=db_host,
+            database=db_name,
+            user=db_user,
+            password=db_password
+        )
+        cursor = conn.cursor()
+
+        # Use EXPLAIN to get the plan
+        execution_plan_query = f"EXPLAIN (analyze, buffers, costs on, FORMAT JSON) {query};"
+        cursor.execute(execution_plan_query)
+
+        # Fetch the plan
+        plan = cursor.fetchall()
+
+        # Close cursor and connection
+        cursor.close()
+        conn.close()
+
+        return plan[0][0][0]['Plan']  # Assume there is at least one plan
+    except psycopg2.Error as e:
+        raise RuntimeError(f"Error getting the execution plan: {e}")
+
+def build_tree_widget_item(plan):
+        """
+        Recursively create QTreeWidgetItem based on the plan node.
+        """
+        item = QTreeWidgetItem([plan['Node Type']])
+        for key, value in plan.items():
+            if key == 'Plans':
+                for child in value:
+                    child_item = build_tree_widget_item(child)
+                    item.addChild(child_item)
+            elif key != 'Node Type':
+                # Add other details as child items
+                child_item = QTreeWidgetItem(["{}: {}".format(key, value)])
+                item.addChild(child_item)
+        return item
+
+def display_tree_image(plan, filename='plan'):
+    """
+    This function visualizes the execution plan using Graphviz launched in a PDF format.
+    """
+    def add_nodes_edges(plan, parent=None):
+        if parent is None:  # Create the root node
+            parent = str(plan['Node Type'])
+            dot.node(parent, label=parent)
+        for child in plan.get('Plans', []):
+            child_node = str(child['Node Type'])
+            dot.node(child_node, label=child_node)
+            dot.edge(parent, child_node)
+            add_nodes_edges(child, parent=child_node)
+    
+    # dot = Digraph(comment='Query Execution Plan')
+
+    # Start adding nodes and edges
+    add_nodes_edges(plan)
+
+    # Save the output
+    # dot.render(filename, view=True)
+    
 def execute_query_in_database(query):
     try:
         # Establish a database connection
--- interface.py
+++ interface.py
@@ -1,8 +1,9 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QTextEdit, QHBoxLayout, QFrame, QScrollArea, QDialog
+from PyQt5.QtWidgets import QApplication, QMessageBox, QWidget, QVBoxLayout, QLabel, QLineEdit, QPushButton, QTextEdit, \
+    QHBoxLayout, QFrame, QScrollArea, QDialog, QTabWidget
 from PyQt5.QtGui import QPalette, QColor, QFont
 
-from explore import execute_query_in_database
+from explore import *
 
 class SQLQueryApp(QWidget):
     def __init__(self):
@@ -13,6 +14,7 @@ class SQLQueryApp(QWidget):
     def initUI(self):
         
         layout_top = QVBoxLayout()
+
         # Create a label for the SQL query input
         self.label = QLabel("SQL Query:")
         layout_top.addWidget(self.label)
@@ -28,6 +30,17 @@ class SQLQueryApp(QWidget):
         # Connect the button click event to a function
         self.execute_button.clicked.connect(self.execute_query)
 
+        # Create a button to visualize the execution plan
+        self.visualize_plan_button = QPushButton("Visualize Execution Plan")
+        layout_top.addWidget(self.visualize_plan_button)
+        self.visualize_plan_button.clicked.connect(self.visualizeQueryPlan)
+
+        # Create a tab widget to hold tabs for each table
+        self.tab_widget = QTabWidget()
+        layout_top.addWidget(self.tab_widget)
+        # Connect the tabChanged signal to a custom function
+        self.tab_widget.currentChanged.connect(self.tabChanged)
+
         # Create a scroll area to make the layout scrollable
         scroll_area = QScrollArea()
         scroll_area.setWidgetResizable(True)  # Ensure the widget inside the scroll area resizes properly
@@ -35,7 +48,7 @@ class SQLQueryApp(QWidget):
         # Create a widget to hold the vertical layout
         layout_bottom_widget = QWidget()
         self.layout_bottom = QVBoxLayout(layout_bottom_widget)
-       
+
         # Set the layout widget as the widget for the scroll area
         scroll_area.setWidget(layout_bottom_widget)
 
@@ -48,17 +61,75 @@ class SQLQueryApp(QWidget):
 
         # Beautify the window with styles
         self.setWindowTitle("SQL Query App")
-        self.setGeometry(100, 100, 600, 400)
+        self.setGeometry(100, 100, 800, 600)
         self.setStyleSheet("background-color: #f0f0f0;")
         self.label.setFont(QFont("Arial", 12))
         self.execute_button.setStyleSheet("background-color: #007acc; color: #ffffff;")
+
+        
+    def displayExecutionPlan(self, plan):
+        # Initialize the QTreeWidget
+        self.plan_tree_widget = QTreeWidget()
+        self.plan_tree_widget.setHeaderLabel("Execution Plan")
+        
+        # Generate the tree items from the plan
+        root_item = build_tree_widget_item(plan)
+        self.plan_tree_widget.addTopLevelItem(root_item)
+        
+        # Add the tree widget to the UI layout
+        self.layout_bottom.addWidget(self.plan_tree_widget)
+        
+        # Update the UI
+        self.plan_tree_widget.expandAll()  # Optionally expand all tree nodes
     
+    def visualizeQueryPlan(self):
+        query = self.sql_input.text()
+        try:
+            plan = get_execution_plan(query)
+            # display_tree_image(plan)
+            self.displayExecutionPlan(plan)
+        except Exception as e:
+            self.showErrorMessage("Error Visualizing Query Plan", str(e))
 
     def execute_query(self):
         # Get the SQL query from the input field
         query = self.sql_input.text()
-        results = execute_query_in_database(query)
-        self.generateBlockAccessedButtons(results)
+        try:
+            # results contains a dictionary of table names and their records
+            self.results = execute_query_in_database(query)
+
+            # for debug purposes only
+            for table_name in self.results:
+                # print(table_name, ": ", results[table_name], "\n\n")
+
+                # Create a new tab for each table
+                tab = QWidget()
+                self.tab_widget.addTab(tab, table_name)
+                self.tab_widget.setCurrentWidget(tab)
+
+        
+
+        except Exception as e:
+            self.showErrorMessage("Error Executing Query", str(e))
+
+    def tabChanged(self, index):
+        # Get the current tab index and perform actions based on it
+        if index >= 0:
+            current_tab = self.tab_widget.widget(index)
+            table_name = self.tab_widget.tabText(index)
+
+            # You can call your function here based on the current tab
+            print(f"Tab changed to {table_name}")
+            # Get the result for the current table
+            result = self.results[table_name]
+            self.generateBlockAccessedButtons(result)
+
+    def showErrorMessage(self, title, message):
+            error_dialog = QMessageBox()
+            error_dialog.setIcon(QMessageBox.Critical)
+            error_dialog.setWindowTitle(title)
+            error_dialog.setText(message)
+            error_dialog.exec_()
     
     def generateBlockAccessedButtons(self, results):
             # Clear existing buttons when executing a new query
